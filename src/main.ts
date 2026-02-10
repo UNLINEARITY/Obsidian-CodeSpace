eb6e765320d7f44297ba2ba8307656cded643260
unlinearity <134848055+UNLINEARITY@users.noreply.github.com>
2026-02-10 15:18:45 +0800

    1.8.0

--- src/main.ts
+++ src/main.ts
@@ -197,7 +197,7 @@ export default class CodeSpacePlugin extends Plugin {
 
 		// Automatically create the outline view in the right sidebar when layout is ready
 		this.app.workspace.onLayoutReady(() => {
-			void this.activateOutlineInSidebar();
+			//void this.activateOutlineInSidebar();
 		});
 	}
 
